e7c9a57eaf653ef76147e313adf2008991fde8bd
Erik2045 <72099875+Erik2045@users.noreply.github.com>
2021-04-11 22:59:15 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation'
+import Portada from './components/Portada'
+import Pokemon from './components/Pokemon'
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navigation></Navigation>
+        <div className="container">
+          <Route path="/" exact component={Portada}></Route>
+          <Route path="/pokemon/:id" component={Pokemon} ></Route>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
